0eff6d3b20cdd817d64d91eaf5f0dd7db58553ec
Alex Yip <alex.yip@softwire.com>
2023-10-02 16:47:59 +0100

    Fix ts

--- src/authorization.ts
+++ src/authorization.ts
@@ -13,6 +13,7 @@ function generateRandomString(length: number) {
 }
 
 function base64encode(stringToEncode: string) {
+  // @ts-ignore
   return btoa(String.fromCharCode.apply(null, new Uint8Array(stringToEncode)))
     .replace(/\+/g, "-")
     .replace(/\//g, "_")
@@ -24,6 +25,7 @@ async function generateCodeChallenge(codeVerifier: string) {
   const data = encoder.encode(codeVerifier);
   const digest = await window.crypto.subtle.digest("SHA-256", data);
 
+  // @ts-ignore
   return base64encode(digest);
 }
 
